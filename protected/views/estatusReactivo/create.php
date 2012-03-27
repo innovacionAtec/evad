e0d10198bb862664ba17a796ac56095d08ad9e87
Patricia Lopez <patricia@patricia-desktop.(none)>
2012-03-27 17:57:20 -0600

    Modificacion de detalle, create, view

--- protected/views/estatusReactivo/create.php
+++ protected/views/estatusReactivo/create.php
@@ -1,4 +1,4 @@
-<?php
+<?php /*
 $this->breadcrumbs=array(
 	'Estatus Reactivos'=>array('index'),
 	'Create',
@@ -7,9 +7,34 @@ $this->breadcrumbs=array(
 $this->menu=array(
 	array('label'=>'List EstatusReactivo', 'url'=>array('index')),
 	array('label'=>'Manage EstatusReactivo', 'url'=>array('admin')),
-);
+);*/
 ?>
 
-<h1>Create EstatusReactivo</h1>
+<div class="row">
+    <div class="span5">
+        <ul class="breadcrumb">
+            <li>
+                <a href="<?php echo CController::createUrl('site/index'); ?>">Inicio</a> <span class="divider">/</span>
+            </li>
+            <li>
+                <a href="<?php echo CController::createUrl('site/catalogos'); ?>">Catálogos</a> <span class="divider">/</span>
+            </li>
+            <li>
+                <a href="<?php echo CController::createUrl('estatusReactivo/index'); ?>">Estatus</a> <span class="divider">/</span>
+            </li>
+            <li class="active">Nuevo</li>
+        </ul>
+    </div>
+</div>
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+<div class="row">
+    <div class="span7">
+        <h1>Agregar Nuevo Estatus</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="span7 offset1">
+        <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+    </div>
+</div>
