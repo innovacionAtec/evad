5378fcd397c00294488de90f7e11ca08404d461e
Patricia Lopez <patricia@patricia-desktop.(none)>
2012-03-27 17:58:14 -0600

    Modificacion de detalle, create, view

--- protected/views/evaluacion/create.php
+++ protected/views/evaluacion/create.php
@@ -1,4 +1,4 @@
-<?php
+<?php /*
 $this->breadcrumbs=array(
 	'Evaluacions'=>array('index'),
 	'Create',
@@ -7,9 +7,34 @@ $this->breadcrumbs=array(
 $this->menu=array(
 	array('label'=>'List Evaluacion', 'url'=>array('index')),
 	array('label'=>'Manage Evaluacion', 'url'=>array('admin')),
-);
+);*/
 ?>
 
-<h1>Create Evaluacion</h1>
+<div class="row">
+    <div class="span5">
+        <ul class="breadcrumb">
+            <li>
+                <a href="<?php echo CController::createUrl('site/index'); ?>">Inicio</a> <span class="divider">/</span>
+            </li>
+            <li>
+                <a href="<?php echo CController::createUrl('site/catalogos'); ?>">Catálogos</a> <span class="divider">/</span>
+            </li>
+            <li>
+                <a href="<?php echo CController::createUrl('evaluacion/index'); ?>">Evaluación</a> <span class="divider">/</span>
+            </li>
+            <li class="active">Nuevo</li>
+        </ul>
+    </div>
+</div>
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+<div class="row">
+    <div class="span7">
+        <h1>Agregar Nueva Evaluación</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="span7 offset1">
+        <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+    </div>
+</div>
