10a1f9d5c2c996ac6b9fe1a659442373cbae6309
Patricia Lopez Garcia <eduviges.lopez@iems.edu.mx>
2012-04-09 17:08:00 -0500

    Actualizacion de estatus, Menu

--- protected/views/estatusReactivo/update.php
+++ protected/views/estatusReactivo/update.php
@@ -1,5 +1,5 @@
 <?php
-$this->breadcrumbs=array(
+/*$this->breadcrumbs=array(
 	'Estatus Reactivos'=>array('index'),
 	$model->id=>array('view','id'=>$model->id),
 	'Update',
@@ -10,9 +10,34 @@ $this->menu=array(
 	array('label'=>'Create EstatusReactivo', 'url'=>array('create')),
 	array('label'=>'View EstatusReactivo', 'url'=>array('view', 'id'=>$model->id)),
 	array('label'=>'Manage EstatusReactivo', 'url'=>array('admin')),
-);
+);*/
 ?>
+<?php $this->pageTitle = CHtml::encode('EVAD | Actualizar estatus'); ?>
+<div class="row">
+    <div class="span5">
+        <ul class="breadcrumb">
+            <li>
+                <a href="<?php echo CController::createUrl('site/index'); ?>">Inicio</a> <span class="divider">/</span>
+            </li>
+            <li>
+                <a href="<?php echo CController::createUrl('site/catalogos'); ?>">Catálogos</a> <span class="divider">/</span>
+            </li>
+            <li>
+                <a href="<?php echo CController::createUrl('estatusReactivo/index'); ?>">Estatus</a> <span class="divider">/</span>
+            </li>
+            <li class="active">Actualización</li>
+        </ul>
+    </div>
+</div>
 
-<h1>Update EstatusReactivo <?php echo $model->id; ?></h1>
+<div class="row">
+    <div class="span7">
+        <h1>Actualizar estatus #<?php echo $model->id; ?></h1>
+    </div>
+</div>
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+<div class="row">
+    <div class="span7 offset1">
+        <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+    </div>
+</div>
