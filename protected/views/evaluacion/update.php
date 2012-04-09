5243f9433323549df9dcd31c4c2aa875c8b24e26
Patricia Lopez Garcia <eduviges.lopez@iems.edu.mx>
2012-04-09 17:16:56 -0500

    Actualizacion de estatus, Menu

--- protected/views/evaluacion/update.php
+++ protected/views/evaluacion/update.php
@@ -1,5 +1,5 @@
 <?php
-$this->breadcrumbs=array(
+/*$this->breadcrumbs=array(
 	'Evaluacions'=>array('index'),
 	$model->id=>array('view','id'=>$model->id),
 	'Update',
@@ -10,9 +10,34 @@ $this->menu=array(
 	array('label'=>'Create Evaluacion', 'url'=>array('create')),
 	array('label'=>'View Evaluacion', 'url'=>array('view', 'id'=>$model->id)),
 	array('label'=>'Manage Evaluacion', 'url'=>array('admin')),
-);
+);*/
 ?>
+<?php $this->pageTitle = CHtml::encode('EVAD | Actualizar evaluación'); ?>
+<div class="row">
+    <div class="span5">
+        <ul class="breadcrumb">
+            <li>
+                <a href="<?php echo CController::createUrl('site/index'); ?>">Inicio</a> <span class="divider">/</span>
+            </li>
+            <li>
+                <a href="<?php echo CController::createUrl('site/catalogos'); ?>">Catálogos</a> <span class="divider">/</span>
+            </li>
+            <li>
+                <a href="<?php echo CController::createUrl('evaluacion/index'); ?>">Evaluación</a> <span class="divider">/</span>
+            </li>
+            <li class="active">Actualización</li>
+        </ul>
+    </div>
+</div>
 
-<h1>Update Evaluacion <?php echo $model->id; ?></h1>
+<div class="row">
+    <div class="span7">
+        <h1>Actualizar evaluación #<?php echo $model->id; ?></h1>
+    </div>
+</div>
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+<div class="row">
+    <div class="span7 offset1">
+        <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+    </div>
+</div>
