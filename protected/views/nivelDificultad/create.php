1aed3b877d11faa851708dbce99e37453ca97ab6
Patricia Lopez Garcia <eduviges.lopez@iems.edu.mx>
2012-04-09 17:34:20 -0500

    Crear nuevo nivel, fecha de creacion y usuario por default

--- protected/views/nivelDificultad/create.php
+++ protected/views/nivelDificultad/create.php
@@ -1,5 +1,5 @@
 <?php
-$this->breadcrumbs=array(
+/*$this->breadcrumbs=array(
 	'Nivel Dificultads'=>array('index'),
 	'Create',
 );
@@ -7,9 +7,34 @@ $this->breadcrumbs=array(
 $this->menu=array(
 	array('label'=>'List NivelDificultad', 'url'=>array('index')),
 	array('label'=>'Manage NivelDificultad', 'url'=>array('admin')),
-);
+);*/
 ?>
+<?php $this->pageTitle = CHtml::encode('EVAD | Nuevo dificultad'); ?>
+<div class="row">
+    <div class="span5">
+        <ul class="breadcrumb">
+            <li>
+                <a href="<?php echo CController::createUrl('site/index'); ?>">Inicio</a> <span class="divider">/</span>
+            </li>
+            <li>
+                <a href="<?php echo CController::createUrl('site/catalogos'); ?>">Catálogos</a> <span class="divider">/</span>
+            </li>
+            <li>
+                <a href="<?php echo CController::createUrl('nivelDificultad/index'); ?>">Nivel de dificultad</a> <span class="divider">/</span>
+            </li>
+            <li class="active">Nuevo</li>
+        </ul>
+    </div>
+</div>
 
-<h1>Create NivelDificultad</h1>
+<div class="row">
+    <div class="span7">
+        <h1>Agregar nuevo nivel de dificultad</h1>
+    </div>
+</div>
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+<div class="row">
+    <div class="span7 offset1">
+        <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+    </div>
+</div>
