1ec5e76b0f7ca84bbab3c2cfac4569fcd04a219a
Patricia Lopez Garcia <eduviges.lopez@iems.edu.mx>
2012-04-09 18:12:45 -0500

    Modificacion del menu y ruta a vistas

--- protected/views/tipoReactivo/update.php
+++ protected/views/tipoReactivo/update.php
@@ -1,5 +1,5 @@
 <?php
-$this->breadcrumbs=array(
+/*$this->breadcrumbs=array(
 	'Tipo Reactivos'=>array('index'),
 	$model->id=>array('view','id'=>$model->id),
 	'Update',
@@ -10,9 +10,34 @@ $this->menu=array(
 	array('label'=>'Create TipoReactivo', 'url'=>array('create')),
 	array('label'=>'View TipoReactivo', 'url'=>array('view', 'id'=>$model->id)),
 	array('label'=>'Manage TipoReactivo', 'url'=>array('admin')),
-);
+);*/
 ?>
+<?php $this->pageTitle = CHtml::encode('EVAD | Actualizar tipo reactivo'); ?>
+<div class="row">
+    <div class="span5">
+        <ul class="breadcrumb">
+            <li>
+                <a href="<?php echo CController::createUrl('site/index'); ?>">Inicio</a> <span class="divider">/</span>
+            </li>
+            <li>
+                <a href="<?php echo CController::createUrl('site/catalogos'); ?>">Catálogos</a> <span class="divider">/</span>
+            </li>
+            <li>
+                <a href="<?php echo CController::createUrl('tipoReactivo/index'); ?>">Tipo de reactivo</a> <span class="divider">/</span>
+            </li>
+            <li class="active">Actualización</li>
+        </ul>
+    </div>
+</div>
 
-<h1>Update TipoReactivo <?php echo $model->id; ?></h1>
+<div class="row">
+    <div class="span7">
+        <h1>Actualizar tipo de reactivo #<?php echo $model->id; ?></h1>
+    </div>
+</div>
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+<div class="row">
+    <div class="span7 offset1">
+        <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+    </div>
+</div>
